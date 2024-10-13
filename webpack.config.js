65261a78cf72d8163fcf2b19d037131ca63ba3f3
JJcode404 <khamjapher1@gmail.com>
2024-10-13 13:25:12 +0300

    feat: resposive home page

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/view.js',
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: './src', 
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,  
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',  
+      inject: 'head',               
+      scriptLoading: 'defer',        
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'],  
       },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', 
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
-},
+  },
 };
